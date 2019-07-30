335cc758cb9f4c1cac7acc633d69dcda1731315d
Mehmet Taşlı <41321646+memolitasli@users.noreply.github.com>
2019-07-30 20:31:33 +0300

    Add files via upload

---
+++ boxhareket.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class boxhareket : MonoBehaviour
+{
+    Rigidbody2D fizik;
+  public  float harekethizi = 50;
+    public Text cantxt;
+    public Text skortxt; 
+    public int skor = 0;
+    public int can = 3;
+    void Start()
+    {
+        fizik = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hareket();
+        skortxt.text = skor.ToString();
+        cantxt.text = can.ToString();
+    }
+
+    void hareket()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+            {
+            fizik.velocity = new Vector2(0, 0);
+            fizik.AddForce(Vector2.left * harekethizi);
+            }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            fizik.velocity = new Vector2(0, 0);
+            fizik.AddForce(Vector2.right * harekethizi);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D nesne)
+    {
+        if(nesne.gameObject.tag == "kirmizi")
+        {
+            Destroy(nesne.gameObject);
+            can--;
+            if(can<= 0)
+            {
+                SceneManager.LoadScene("lvl1");
+            }
+
+        }
+        if(nesne.gameObject.tag == "mavi")
+        {
+            Destroy(nesne.gameObject);
+            skor++;
+        }
+    }
+}
---
+++ oyunkontrol.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oyunkontrol : MonoBehaviour
+{
+   
+    public GameObject kirmizi;
+    public GameObject mavi;
+    public GameObject arkaplan1;
+    public GameObject arkaplan2;
+    
+
+    int x1 = -8;
+    int x2 = 0;
+    int x3 = 8;
+    int sayac = 1;
+    
+    public int can = 3;
+    public int skor = 0;
+    float zaman = 0;
+    float arkaplanuzunluk = 0;
+
+    Rigidbody2D f1;
+    Rigidbody2D f2;
+    Rigidbody2D fizik1;
+    Rigidbody2D fizik2;
+    
+    void Start()
+    {
+      
+
+    }
+
+
+   
+
+    void FixedUpdate()
+    {
+        kutuolustur();
+        
+
+    }
+
+     void kutuolustur()
+    {
+        if (sayac == 1)
+        {
+            int sayi = Random.Range(0, 3);
+
+
+            if (sayi == 0)
+            {
+                Instantiate(mavi, new Vector2(x1, 4), Quaternion.identity);
+                Instantiate(kirmizi, new Vector2(x2, 4), Quaternion.identity);
+                Instantiate(kirmizi, new Vector2(x3, 4), Quaternion.identity);
+
+            }
+            else if (sayi == 1)
+            {
+                Instantiate(mavi, new Vector2(x2, 4), Quaternion.identity);
+                Instantiate(kirmizi, new Vector2(x1, 4), Quaternion.identity);
+                Instantiate(kirmizi, new Vector2(x3, 4), Quaternion.identity);
+
+            }
+            else if (sayi == 2)
+            {
+                Instantiate(mavi, new Vector2(x3, 4), Quaternion.identity);
+                Instantiate(kirmizi, new Vector2(x2, 4), Quaternion.identity);
+                Instantiate(kirmizi, new Vector2(x1, 4), Quaternion.identity);
+
+            }
+
+
+        }
+        sayac = 0;
+        zaman += Time.deltaTime;
+        if (zaman >= 3)
+        {
+            sayac = 1;
+            zaman = 0;
+        }
+    }
+
+
+   
+}
